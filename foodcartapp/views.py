d135c08e5740c0142577e172b039a7a208cce763
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-07-26 22:33:49 +0500

    Setup DRF

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,9 +1,10 @@
-import json
-
 from django.http import JsonResponse
 from django.templatetags.static import static
 
 from .models import Order, Product
+from .serializers import OrderSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 def banners_list_api(request):
@@ -70,8 +71,9 @@ def product_list_api(request):
     )
 
 
+@api_view(["POST"])
 def register_order(request):
-    data = json.loads(request.body.decode())
+    data = request.data
 
     order = Order.objects.create(
         first_name=data["firstname"],
@@ -82,4 +84,6 @@ def register_order(request):
 
     for product in data["products"]:
         order.order_items.create(product=Product.objects.get(pk=product["product"]), quantity=product["quantity"])
-    return JsonResponse({})
+
+    response_data = OrderSerializer(order).data
+    return Response(response_data)
--- star_burger/settings.py
+++ star_burger/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "phonenumber_field",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
