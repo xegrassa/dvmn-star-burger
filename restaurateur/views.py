53f56d51a5c9504b549691e4382f69d1b53ac50d
Артем Нетрибейчук <artem.netribeychuk@skillbox.ru>
2024-07-30 19:46:36 +0500

    List orders into manager UI

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import View
 
-from foodcartapp.models import Product, Restaurant
+from foodcartapp.models import Product, Restaurant, Order
 
 
 class Login(forms.Form):
@@ -96,10 +96,12 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url="restaurateur:login")
 def view_orders(request):
+    orders = Order.objects.all()
+
     return render(
         request,
         template_name="order_items.html",
         context={
-            # TODO заглушка для нереализованного функционала
+            "order_items": orders
         },
     )
